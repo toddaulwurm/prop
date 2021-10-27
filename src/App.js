8d35c2a50ceb5ca5677053ac65011072f7215728
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-10-27 11:37:25 -0500

    capitalized components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import PersonCard from './components/prop';
+import PersonCard from './components/Prop';
 
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <PersonCard firstName={"Jane"} lastName="Doe" age={45} hairColor="Black"/>
       <PersonCard firstName={"John"} lastName="Smith" age={88} hairColor="Brown"/>  
       <PersonCard firstName={"Millard"} lastName="Fillmore" age={50} hairColor="Brown"/>  
