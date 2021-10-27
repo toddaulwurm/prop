5e13d1f741e6cbcd073e2716acf7252d40420b40
toddaulwurm <85901690+toddaulwurm@users.noreply.github.com>
2021-10-27 13:39:48 -0500

    plusSTATE and SYNTH EVENTS

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import PersonCard from './components/Prop';
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName={"Jane"} lastName="Doe" age={45} hairColor="Black"/>
+      {/* PARAGRAPH is a CHILD of the PERSONCARD                                     vvvvv*/}
+      <PersonCard firstName={"Jane"} lastName="Doe" age={45} hairColor="Black"><h1>PARAGRAPH</h1></PersonCard>
       <PersonCard firstName={"John"} lastName="Smith" age={88} hairColor="Brown"/>  
       <PersonCard firstName={"Millard"} lastName="Fillmore" age={50} hairColor="Brown"/>  
-      <PersonCard firstName={"Maria"} lastName="Smith" age={62} hairColor="Brown"/>     
+      <PersonCard firstName={"Maria"} lastName="Smith" age={62} hairColor="Brown"/>
+          {/* below is a synthetic event!!! NOTE: Camel Casing */}
+      <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button>
     </div>
   );
 
--- src/components/prop.js
+++ src/components/prop.js
@@ -2,12 +2,30 @@ import React, { Component } from 'react';
     
     
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: this.props.age
+        };
+    }
+    // BELOW IS USED TO MANIPULATE STATE
+    birthday = () => {
+        this.setState({age : this.state.age+1})
+        console.log(this.state.age);
+    }
     render() {
-        const { firstName, lastName, age, hairColor} = this.props;
+        // CHILDREN IS PROPPED HERE                   vvvvvvv
+        const { firstName, lastName, hairColor, children} = this.props;
         return <div>
                     <h1>{ lastName }, {firstName}</h1>
-                    <p>Age: {age}</p>
+                    <p>Age: {this.state.age}</p>
                     <p>Hair Color: {hairColor}</p>
+
+                    {/* and CHILDREN is called below */}
+                    <p>{children}</p>
+
+                    {/* Example of setiing STATE below with a SYNTHETIC EVENT */}
+                    <button onClick={ this.birthday }>Birthday Button for {firstName} {lastName}</button>
                 </div>;
     }
 }
